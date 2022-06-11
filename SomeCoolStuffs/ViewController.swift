bf8103d80ce9767b5ce88db1fca4212e2708c63c
kneskoromny <kneskoromny@gmail.com>
2022-06-11 14:11:44 +0300

    Initial Commit

---
+++ SomeCoolStuffs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SomeCoolStuffs
+//
+//  Created by Кирилл Нескоромный on 11.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
